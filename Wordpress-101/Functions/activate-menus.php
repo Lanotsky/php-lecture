12a0e1c928de4b3e1e4f00c49cdba620f0be88f1
Aaron James <aaronlanot@gmail.com>
2018-08-20 10:53:01 +0800

    alessandro castellani lecture

---
+++ Wordpress-101/Functions/activate-menus.php
@@ -0,0 +1,16 @@
+<?php
+/*
+	==========================================
+	 Activate menus
+	==========================================
+*/
+function awesome_theme_setup() {
+	
+	add_theme_support('menus');
+	
+	register_nav_menu('primary', 'Primary Header Navigation');
+	register_nav_menu('secondary', 'Footer Navigation');
+	
+}
+add_action('init', 'awesome_theme_setup');
+?>
